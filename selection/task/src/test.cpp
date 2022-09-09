f70e649593a08fad69688285546d98e931bdb7a1
imaximus3 <max2000.gym5cheb@yandex.ru>
2022-09-09 21:41:19 +0300

    system_item table

--- selection/task/src/test.cpp
+++ selection/task/src/test.cpp
@@ -4,5 +4,16 @@
 void Test::TestDB()
 {
     Database db;
-    db.ExecuteSQL("create table if not exists users(id int, name text);");
+    db.ExecuteSQL(
+            "CREATE TABLE IF NOT EXISTS system_item("
+            "uid SERIAL PRIMARY KEY,"
+            "id VARCHAR(255) NOT NULL UNIQUE,"
+            "url VARCHAR(255),"
+            "date VARCHAR(255) NOT NULL,"
+            "dt_date TIMESTAMP NOT NULL,"
+            "parentId VARCHAR(255) REFERENCES system_item (id),"
+            "type VARCHAR(255) NOT NULL,"
+            "size BIGINT"
+            ");"
+    );
 }
