2aa0f7b3d9b12ad0aad05bbd68904becc10bcd30
maximus3 <max2000.gym5cheb@yandex.ru>
2022-10-28 15:55:09 +0300

    hw1 fix

--- homeworks/homework_01/tests/test_basic.py
+++ homeworks/homework_01/tests/test_basic.py
@@ -1,20 +1,28 @@
 import pytest
 
-from testsuite.databases import pgsql
-
-
 # Start the tests via `make test-debug` or `make test-release`
 
-
 async def test_basic(service_client):
-    response = await service_client.post('/v1/make-shorter', data='{"url": "http://example.com?id=123"}')
+    data = {"url": "http://example.com?id=123"}
+    response = await service_client.post('/v1/make-shorter', json=data)
     assert response.status == 200
     
     response_json = response.json()
     assert 'short_url' in response_json
 
-    request_uri = response_json['short_url']
-    request_uri = request_uri[request_uri.rfind('/'):]
+    request_url = response_json['short_url']
+    request_url = request_url[request_url.rfind('/'):]
 
-    response = await service_client.get(request_uri)
+    response = await service_client.get(request_url)
     assert response.status == 200
+
+
+async def test_bad_request(service_client):
+    data = {}
+    response = await service_client.post('/v1/make-shorter', json=data)
+    assert response.status == 400
+
+
+async def test_redirect_not_found(service_client):
+    response = await service_client.get('/unknown-id')
+    assert response.status == 404
